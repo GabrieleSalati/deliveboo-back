7c54dc16e419b51f43055c997bf9d879ca584bd5
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-05-15 18:34:36 +0200

    fix redirect after login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\Restaurant;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -23,13 +24,16 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): RedirectResponse
+    public function store(LoginRequest $request)
+    // : RedirectResponse
     {
         $request->authenticate();
-
+        $user_data = Auth::user();
+        $restaurant = Auth::user()->restaurant;
         $request->session()->regenerate();
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+        return view('admin.restaurants.show',compact('restaurant', 'user_data'));
+        // return redirect()->intended(RouteServiceProvider::HOME);
     }
 
     /**
