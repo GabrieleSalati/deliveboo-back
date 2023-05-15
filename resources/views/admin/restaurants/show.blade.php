cbc577aa929f0aa6f2d2cd9fe61017054986b65c
SofiaCurreri <sofiacurreri@gmail.com>
2023-05-15 17:00:09 +0200

    user email now visible

--- resources/views/admin/restaurants/show.blade.php
+++ resources/views/admin/restaurants/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
            
-        <section class="card" style="width: 18rem;">
+        <section class="card my-5" style="width: 22rem;">
             <img src="{{$restaurant->getImageUri()}}" class="card-img-top img-fluid" alt="restaurant image">
             <div class="card-body">
                 <h5 class="card-title mb-3">{{$restaurant->restaurant_name}}</h5>
@@ -15,7 +15,7 @@
                 </p>
 
                 <p class="card-text">
-                    <strong> Email: </strong> {{$restaurant->users()->email}}
+                    <strong> Email: </strong> {{$user_data['email']}}
                 </p>                 
 
                 <a href="#" class="btn btn-primary">Il mio Menù</a>
