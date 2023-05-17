54b57fa0289b1176ee3bfafa86133fb4366fceee
GabrieleSalati <gabriele.salati5@gmail.com>
2023-05-17 10:01:24 +0200

    Added route for restaurants api

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::apiResource('restaurants', RestaurantController::class);
 Route::apiResource('categories', CategoryController::class);
