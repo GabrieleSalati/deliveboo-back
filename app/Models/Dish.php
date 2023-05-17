ab3315b571cb1ab678677024d442239acc7ac393
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-05-17 23:25:55 +0200

    added getpictureuri in dish model

--- app/Models/Dish.php
+++ app/Models/Dish.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Str;
 class Dish extends Model
 {
     use HasFactory;
@@ -21,4 +21,12 @@ class Dish extends Model
     {
         return $this->belongsTo(Restaurant::class);
     }
+    
+    public function getPictureUri() {
+      if(!empty($this->picture)) {
+        if(Str::startsWith($this->picture, 'http')) return $this->picture; 
+        else return url('storage/' . $this->picture);
+      }
+      else return 'https://www.frosinonecalcio.com/wp-content/uploads/2021/09/default-placeholder.png';
+    } 
 }
