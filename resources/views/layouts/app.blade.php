e3f46c6a9f316f837cc78a9137dd7dd3502eeb91
SofiaCurreri <sofiacurreri@gmail.com>
2023-05-18 10:04:44 +0200

    took away home and crea piatto from navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,17 +41,17 @@
 					<span class="navbar-toggler-icon"></span>
 				</button>
 
-				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
 					<!-- Left Side Of Navbar -->
 					<ul class="navbar-nav me-auto">
-						<li class="nav-item">
+						{{-- <li class="nav-item">
 							<a class="nav-link" href="{{ url('/') }}">{{ __('Home') }}</a>
 						</li>
 
 						<li class="nav-item">
 							<a class="nav-link" href="{{ route('dishes.create') }}">{{ __('Crea piatto') }}</a>
-						</li>
-					</ul>
+						</li> --}}
+					</ul> 
 
 					<!-- Right Side Of Navbar -->
 					<ul class="navbar-nav ml-auto">
