d904841e8c6dcfc2ce8a7151df37bbc76efb064b
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-05-19 15:53:15 +0200

    dish ordered by name in dish index

--- app/Http/Controllers/DishController.php
+++ app/Http/Controllers/DishController.php
@@ -21,7 +21,7 @@ class DishController extends Controller
     public function index()
     {
       $restaurant_id = Auth::user()->restaurant->id;
-      $dishes = Dish::where('restaurant_id', $restaurant_id)->get();
+      $dishes = Dish::where('restaurant_id', $restaurant_id)->orderBy("name", "ASC")->get();
   
       return view('admin.dishes.index')->with('dishes', $dishes);
     }
